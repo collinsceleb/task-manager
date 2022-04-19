cebe0282da0634839002489910bf809b3eb42a30
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2022-04-19 15:45:04 +0100

    Connect to MongoDB using mongoose

---
+++ controllers/tasks.js
@@ -0,0 +1,35 @@
+
+
+
+const getAllTasks = (req, res) => {
+    res.send("get all tasks");
+}
+
+const getTask = (req, res) => {
+    // res.send("get task");
+    res.json({ id: req.params.id})
+}
+
+const createTask = (req, res) => {
+//   res.send("create task");
+    res.json(req.body)
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+
+
+
+
+module.exports = {
+    getAllTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+}
