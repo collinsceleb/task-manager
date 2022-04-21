9c914a1c042c4842ab782c8daa724145952d6fa6
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2022-04-21 18:33:18 +0100

    Create public folders with files.

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const port = 3000
 
 
 // middleware
+app.use(express.static("./public"))
 app.use(express.json());
 
-// routes
-app.get('/hello', (req, res) => {
-    res.send("Task Manager App")
-});
+// // routes
+// app.get('/hello', (req, res) => {
+//     res.send("Task Manager App")
+// });
 
 app.use("/api/v1/tasks", tasks);
 
